21f9fb1f96c7848017575bceb9062be88215ce6e
huntermacias <supercodersf@gmail.com>
2024-01-24 17:58:09 -0800

    bug: updated review interface

--- src/pages/api/reviews.ts
+++ src/pages/api/reviews.ts
@@ -1,44 +1,49 @@
-import { db } from '@/lib/db';
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { db } from '@/lib/db';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { PrismaClient } from '@prisma/client';
 
-// api/reviews.ts
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-      res.setHeader('Allow', ['POST']);
-      return res.status(405).end('Method Not Allowed');
-  }
 
-  // Hardcoded values for testing
-  const clerkUserId = 'user_2bPc3HCyrCaKS8piwsvNhNTjLEW';
-  const movieId = 787699;
-  const body = 'awful';
-  const rating = 3;
+// const prisma = new PrismaClient();
 
-  try {
-      // Check if the user exists
-      const user = await db.user.findUnique({
-          where: { clerkUserId: clerkUserId },
-      });
+// // api/reviews.ts
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method !== 'POST') {
+//       res.setHeader('Allow', ['POST']);
+//       return res.status(405).end('Method Not Allowed');
+//   }
 
-      if (!user) {
-          return res.status(404).json({ error: 'User not found' });
-      }
+//   // Hardcoded values for testing
+//   const clerkUserId = 'user_2bPc3HCyrCaKS8piwsvNhNTjLEW';
+//   const movieId = 787699;
+//   const body = 'awful';
+//   const rating = 3;
 
-      // Create the review
-      const review = await db.review.create({
-          data: {
-              movieId, // Assuming this is an integer in your database
-              body,
-              rating,
-              clerkUserId, // Directly use clerkUserId since it's already a string
-          },
-      });
+//   try {
+//       // Check if the user exists
+//       const user = await db.user.findUnique({
+//           where: { clerkUserId: clerkUserId },
+//       });
 
-      return res.status(200).json(review);
-  } catch (error: any) {
-      console.error('Error:', error);
-      return res.status(500).json({ message: 'Internal Server Error', error: error.message });
-  }
-};
+//       if (!user) {
+//           return res.status(404).json({ error: 'User not found' });
+//       }
 
-export default handler;
+//       // Create the review
+//       const review = await db.review.create({
+//           data: {
+//               movieId, // Assuming this is an integer in your database
+//               body,
+//               rating,
+//               clerkUserId: user.clerkUserId,
+//           },
+//       });
+
+//       return res.status(200).json(review);
+//   } catch (error: any) {
+//       console.error('Error:', error);
+//       return res.status(500).json({ message: 'Internal Server Error', error: error.message });
+//   }
+// };
+
+// export default handler;
+
